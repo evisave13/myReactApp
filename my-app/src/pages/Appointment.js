c40383ea46834adafb529088337385ac51aaf839
Ekant Visave <ekantlvisave@gmail.com>
2024-11-14 23:09:43 +0530

    changes

---
+++ my-app/src/pages/Appointment.js
@@ -0,0 +1,13 @@
+// src/pages/Appointments.js
+import React from 'react';
+
+function Appointment() {
+  return (
+    <div>
+      <h1>Appointments</h1>
+      <p>This is the Appointments page where users can schedule or view appointments.</p>
+    </div>
+  );
+}
+
+export default Appointment;
---
+++ my-app/src/pages/Home.js
@@ -0,0 +1,13 @@
+// src/pages/Home.js
+import React from 'react';
+
+function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to Our Clinic</h1>
+      <p>Your health is our priority.</p>
+    </div>
+  );
+}
+
+export default Home;
